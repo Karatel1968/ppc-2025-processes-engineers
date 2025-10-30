b1039f930c9cbe6c7529107b1ee9e84789ffad03
Oleg Urin <olegurin76@mail.ru>
2025-10-30 17:52:58 +0300

    Add test task

---
+++ tasks/urin_o_max_val_in_col_of_mat/common/include/common.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+#include "task/include/task.hpp"
+
+namespace urin_o_max_val_in_col_of_mat {
+
+using InType = int;
+using OutType = int;
+using TestType = std::tuple<int, std::string>;
+using BaseTask = ppc::task::Task<InType, OutType>;
+
+}  // namespace urin_o_max_val_in_col_of_mat
---
+++ tasks/urin_o_max_val_in_col_of_mat/tests/performance/main.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+
+#include "urin_o_max_val_in_col_of_mat/common/include/common.hpp"
+#include "urin_o_max_val_in_col_of_mat/mpi/include/ops_mpi.hpp"
+#include "urin_o_max_val_in_col_of_mat/seq/include/ops_seq.hpp"
+#include "util/include/perf_test_util.hpp"
+
+namespace urin_o_max_val_in_col_of_mat {
+
+class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+  const int kCount_ = 100;
+  InType input_data_{};
+
+  void SetUp() override {
+    input_data_ = kCount_;
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return input_data_ == output_data;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+};
+
+TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
+
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, UrinOMaxValInColOfMatMPI, UrinOMaxValInColOfMatSEQ>(PPC_SETTINGS_urin_o_max_val_in_col_of_mat);
+
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
+const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+
+}  // namespace urin_o_max_val_in_col_of_mat
